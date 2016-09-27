c7e8818d305865f27d1d082b3433f9e4e0f20e9c
AlekseyPn <aleksfers@icloud.com>
2016-09-27 20:00:37 +0500

    Work at JS

---
+++ js/script.js
@@ -0,0 +1,75 @@
+// Script index page
+// index page variable
+var overlay = document.querySelector(".modal-overlay");
+var openModal = document.querySelectorAll(".item-open");
+var popup = document.querySelector(".modal-content");
+var close = document.querySelectorAll(".item-close");
+var mapPopup = document.querySelector(".modal-content__map");
+var openMap = document.querySelector(".map");
+var userName = document.querySelector("[name='name']");
+var btnSlide = document.querySelectorAll("#btn-slide");
+var slide = document.querySelectorAll(".slide");
+var currentSlide = 0;
+
+for (i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.add("modal-content--show");
+        overlay.classList.add("modal-overlay--show");
+        userName.focus();
+    });
+}
+
+for (i = 0; i < close.length; i++) {
+    close[i].addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.remove("modal-content--show");
+        overlay.classList.remove("modal-overlay--show");
+        mapPopup.classList.remove("modal-map--show");
+    });
+};
+
+openMap.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapPopup.classList.add("modal-map--show");
+    overlay.classList.add("modal-overlay--show");
+});
+
+overlay.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-content--show");
+    overlay.classList.remove("modal-overlay--show");
+    mapPopup.classList.remove("modal-map--show");
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (popup.classList.contains("modal-content--show")) {
+            popup.classList.remove("modal-content--show");
+            overlay.classList.remove("modal-overlay--show");
+            popup.classList.remove("modal-error");
+        };
+        if (mapPopup.classList.contains("modal-map--show")) {
+            mapPopup.classList.remove("modal-map--show");
+            overlay.classList.remove("modal-overlay--show");
+        };
+    };
+});
+
+// Slider index page
+
+function nextSlide() {
+    goToSlide(currentSlide + 1);
+};
+
+
+function goToSlide(n) {
+    slide[currentSlide].className = "slide  slide-show"
+    currentSlide = (n + slide.length) % slide.length;
+    slide[currentSlide].className = "slide";
+};
+for (i = 0; i < btnSlide.length; i++) {
+    btnSlide[i].addEventListener("click", function() {
+        nextSlide();
+    });
+};
